39636fdc7b836ac59cb33cb37ebdf276bc25a88f
Nishad <nishuanshad@gmail.com>
2023-10-28 14:14:03 +0530

    unwanted log removed

--- back_end/controller.js
+++ back_end/controller.js
@@ -17,7 +17,6 @@ export async function getMovie(req,res){
 
 export async function getDetails(req,res){
    const{id}=req.params;
-   console.log(id);
    let Movie=await schema.findOne({_id:id})
    console.log(Movie);
    res.status(200).send(Movie)
